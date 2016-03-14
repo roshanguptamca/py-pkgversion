6154f4d929b2d0117a9696a12455fc80abfd8264
Niels Lensink <niels.lenssink@kpn.com>
2016-03-14 12:16:07 +0100

    OPT keep the github repo with the py prefix

--- setup_gen.py
+++ setup_gen.py
@@ -24,6 +24,8 @@ write_setup_py(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
